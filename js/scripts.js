ad109ccef0cf5e6f97c73eff67d09cf6e2a9f11d
grinil khanna, Minh Phuong and Tammy Dang <tammy@>
2017-01-19 16:01:59 -0800

    Define for loop to get first column

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,29 @@
 var row = 0;
 var col = 0;
 var fake = 0;
+var array = [];
 
 function convert () {
   fake = Math.sqrt(Input.length);
+  var m = Input.length;
+  console.log(fake);
+  console.log(m);
  if ( fake === parseInt(fake,10) ) {
    row = fake;
    col = fake;
+  for (var i = 0; i < m ; i+=col) {
+     console.log(array[i]);
+   }
+
 
  } else {
-   alert("blah");
-   console.log(Input.length);
    row = Math.round(fake) + 1;
    col = Math.round(fake);
-
-
-
+   console.log(row);
+   console.log(col);
+   for (var i = 0; i < m ; i+=col) {
+     console.log(array[i]);
+   }
 }
 };
 
@@ -25,8 +33,10 @@ $("document").ready(function() {
   $(".form-group").submit(function(event) {
     event.preventDefault();
     Input = $("#userInput").val();
+    array = Input.split("");
+    console.log(array);
     $(".result").show();
     convert();
-    $("#crypt").text();
+    $("#crypt").text(array);
   });
 });
